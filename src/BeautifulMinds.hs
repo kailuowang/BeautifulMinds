a1e1a5e61366e8b5565bbe07a9caeeebf881e1cf
kailuo wang <kailuo.wang@gmail.com>
2015-01-23 22:25:37 -0500

    refactor to do create or update

--- src/BeautifulMinds.hs
+++ src/BeautifulMinds.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-module BeautifulMinds where
+module BeautifulMinds(recordFave, perform) where
 import WeightedSlopeOne
 import Database.Neo4j
 import qualified Data.HashMap.Lazy as M
@@ -9,35 +9,67 @@ import Data.Maybe (listToMaybe)
 type FaveRating = Int64
 type PhotographerId = Text
 type PhotoId = Text
+type NodeId = Text
 type FavedById = PhotographerId
 type FaveRecord = (FavedById, FaveRating, PhotographerId, PhotoId)
+
+photographerLabel :: Label
+photographerLabel = "Photographer"
+photoLabel :: Label
+photoLabel = "Photo"
+
 
 recordFave :: FaveRecord -> IO Relationship
 recordFave (favedBy, rating, photographerId, photoId) = perform $ do
   by <- savePhotographer favedBy
   photo <- savePhoto photoId photographerId
-  favePhoto rating photo by
+  favePhoto rating by photo
 
-getPhotographer :: PhotographerId -> Neo4j (Maybe Node)
-getPhotographer pid = fmap listToMaybe $ getNodesByLabelAndProperty "Photographer" $ Just ("id" |: pid)
+findById :: Label -> NodeId -> Neo4j (Maybe Node)
+findById label nid =  fmap listToMaybe $ getNodesByLabelAndProperty label $ Just ("id" |: nid)
 
+create :: Label -> NodeId -> Neo4j Node
+create label nid = do
+    node <- createNode $ M.fromList [ "id" |: nid ]
+    addLabels [label] node
+    return node
+
+findOrCreate :: Label -> NodeId -> Neo4j Node
+findOrCreate label nid = do
+  node <- findById label nid
+  case node of Just n -> return n
+               Nothing -> create label nid
+
+updateOrCreate :: RelationshipType -> Node -> Node -> Properties -> Neo4j Relationship
+updateOrCreate relType fromNode toNode props = do
+  fromRels <- getRelationships fromNode Outgoing [relType]
+  toRels <- getRelationships toNode Incoming [relType]
+  case common fromRels toRels of [] -> createRelationship relType props fromNode toNode
+                                 x : _ -> return x
 
 savePhotographer :: PhotographerId -> Neo4j Node
-savePhotographer pid = do
-  node <- createNode $ M.fromList [ "id" |: pid ]
-  addLabels ["Photographer"] node
-  return node
+savePhotographer = findOrCreate photographerLabel
 
 savePhoto :: PhotoId -> PhotographerId -> Neo4j Node
 savePhoto photoId photographerId = do
-  photo <- createNode $ M.fromList [ "id" |: photoId ]
-  addLabels ["Photo"] photo
-  photographer <- savePhotographer photographerId
-  createRelationship "TakenBy" M.empty photo photographer
-  return photo
+    maybePhoto <- findById photoLabel photoId
+    case maybePhoto of Just p -> return p
+                       Nothing -> createPhoto photoId photographerId
+
+createPhoto :: PhotoId -> PhotographerId -> Neo4j Node
+createPhoto photoId photographerId = do
+   photo <- create photoLabel photoId
+   photographer <- savePhotographer photographerId
+   createRelationship "Takes" M.empty photographer photo
+   return photo
 
 favePhoto :: FaveRating -> Node -> Node -> Neo4j Relationship
-favePhoto rating = createRelationship "Fave" (M.fromList [ "rating" |: rating])
+favePhoto rating by photo = do
+  updateOrCreate "Fave" by photo (M.fromList [ "rating" |: rating])
+
+common :: (Eq a) => [a] -> [a] -> [a]
+common xs ys = [ x | x <- xs , y <- ys, x == y]
+
 
 
 perform :: Neo4j a -> IO a
--- tests/BeautifulMindsSpec.hs
+++ tests/BeautifulMindsSpec.hs
@@ -12,6 +12,12 @@ spec :: Spec
 spec = do
   describe "record fave " $ do
     it "store the relationship" $ do
-      faveRel <- recordFave ("favedby", 3, "aPhotographer", "aPhoto")
+      faveRel <- recordFave ("aUser", 3, "aPhotographer", "aPhoto")
       let rating = getRelProperties faveRel M.! "rating"
       rating `shouldBe` (ValueProperty $ IntVal 3)
+      (user, photo) <-  perform $ do
+        from <- getRelationshipFrom faveRel
+        to <- getRelationshipTo faveRel
+        return (from, to)
+      getNodeProperties user M.! "id" `shouldBe` (ValueProperty $ TextVal "aUser")
+      getNodeProperties photo M.! "id" `shouldBe` (ValueProperty $ TextVal "aPhoto")
